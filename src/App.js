9362d51b8890e828f8d9a83cad09accd1997dcd6
Ezkenneth <kennethjoeezigbo@gmail.com>
2021-04-09 09:00:52 -0400

    created react app - ezcoding

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Background from "./images/old-black-background.jpg"
+
+function App() {
+  return (
+    <Router>
+      <div style={{backgroundImage: `url(${Background})`,
+    height: "100vh",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat"
+    }}>
+      
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} />
+        
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+/* .homepage-bgimage {
+    background: url("/src/images/old-black-background.jpg");
+    height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  } */
---
+++ src/components/Backgroundcomponent.js
@@ -0,0 +1,10 @@
+// import React, { Component } from "react";
+// import "../styles/background.css"
+
+// export default class bg extends Component {
+//   render() {
+//     return (
+//         <div className="container-fluid homepage-bgimage"></div>
+//     );
+//   }
+// }
